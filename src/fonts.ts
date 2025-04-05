b8d765a24415476432e5bb2d547b5e66de061899
junwookwon <kwonj5549@gmail.com>
2025-04-04 22:31:48 -0700

    font change

--- src/fonts.ts
+++
@@ -1,8 +0,0 @@
-// src/fonts.ts
-import { Noto_Sans_KR } from "next/font/google";
-
-export const notoSansKR = Noto_Sans_KR({
-    weight: ["400", "700"],
-    subsets: ["latin", "korean"],
-    variable: "--font-noto-sans-kr",
-});
